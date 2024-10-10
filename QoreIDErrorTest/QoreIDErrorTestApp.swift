7d5acdc5aabc30504603a37c6f72a7ecbd7a673b
onyekvchi <onyekachi.mbaike@gmail.com>
2024-10-10 11:13:04 +0100

    Initial Commit

---
+++ QoreIDErrorTest/QoreIDErrorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QoreIDErrorTestApp.swift
+//  QoreIDErrorTest
+//
+//  Created by Onyekachi Mbaike on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct QoreIDErrorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
